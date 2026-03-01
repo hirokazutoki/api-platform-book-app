83f7f9321977a527b10c124bb99d3c138fc90921
hirokazutoki <hrkztk1124@gmail.com>
2026-03-01 23:07:34 +0900

    Specify custom table name for `Comment` entity in Doctrine configuration

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -8,6 +8,7 @@ use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: CommentRepository::class)]
+#[ORM\Table(name: 'comments')]
 #[ApiResource]
 class Comment
 {
