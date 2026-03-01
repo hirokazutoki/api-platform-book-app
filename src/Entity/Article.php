1ad881596b7f8e616e4bb6602acb3501913e89a6
hirokazutoki <hrkztk1124@gmail.com>
2026-03-01 22:56:17 +0900

    Specify custom table name for `Article` entity in Doctrine configuration

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -8,6 +8,7 @@ use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: ArticleRepository::class)]
+#[ORM\Table(name: 'articles')]
 #[ApiResource]
 class Article
 {
