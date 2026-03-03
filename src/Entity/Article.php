8fcb20746d75d8d470199225a8ac3703370fd450
hirokazutoki <hrkztk1124@gmail.com>
2026-03-03 13:19:23 +0900

    Add validation constraints to `Article` and `Comment` entities

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -8,6 +8,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: ArticleRepository::class)]
 #[ORM\Table(name: 'articles')]
@@ -20,12 +21,15 @@ class Article
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank]
+    #[Assert\Length(max: 255)]
     private ?string $title = null;
 
     #[ORM\Column(type: Types::TEXT, nullable: true)]
     private ?string $content = null;
 
     #[ORM\Column]
+    #[Assert\NotNull]
     private ?bool $published = null;
 
     /**
