51a7aea334371b4b92c0a6f38990edd82296ca93
hirokazutoki <hrkztk1124@gmail.com>
2026-03-09 22:06:43 +0900

    add: custom OpenApiFactory to modify requestBody for article publication endpoint

---
+++ src/ApiPlatform/OpenApi/Factory/OpenApiFactory.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\ApiPlatform\OpenApi\Factory;
+
+use ApiPlatform\OpenApi\Factory\OpenApiFactoryInterface;
+use ApiPlatform\OpenApi\Model;
+use ApiPlatform\OpenApi\OpenApi;
+use Symfony\Component\DependencyInjection\Attribute\AsDecorator;
+
+#[AsDecorator(decorates: 'api_platform.openapi.factory')]
+class OpenApiFactory implements OpenApiFactoryInterface
+{
+    public function __construct(private OpenApiFactoryInterface $decorated)
+    {
+    }
+
+    public function __invoke(array $context = []): OpenApi
+    {
+        $openApi = $this->decorated->__invoke($context);
+
+        foreach ($openApi->getPaths()->getPaths() as $path => $pathItem) {
+            assert(is_string($path));
+            assert($pathItem instanceof Model\PathItem);
+
+            foreach (Model\PathItem::$methods as $method) {
+                $getter = sprintf('get%s', ucfirst(strtolower($method)));
+                $wither = sprintf('with%s', ucfirst(strtolower($method)));
+                $operation = $pathItem->{$getter}();
+                assert($operation instanceof Model\Operation || $operation === null);
+
+                if ($operation === null) {
+                    continue;
+                }
+
+                // PUT /api/articles/{id}/publication ではリクエストボディを削除
+                if (strtolower($method) === 'put' && $path === '/api/articles/{id}/publication') {
+                    $operation = $operation->withrequestBody();
+                }
+
+                $pathItem = $pathItem->{$wither}($operation);
+
+                $openApi->getPaths()->addPath($path, $pathItem);
+            }
+        }
+
+        return $openApi;
+    }
+}
