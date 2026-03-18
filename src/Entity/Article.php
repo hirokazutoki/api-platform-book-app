45e4942ab761af420c912d3235fe3c70d12613a4
hirokazutoki <hrkztk1124@gmail.com>
2026-03-18 12:54:25 +0900

    add: enable Timestampable behavior in Article entity with StofDoctrineExtensions configuration

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -20,6 +20,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Timestampable\Traits\TimestampableEntity;
 use Symfony\Component\Serializer\Attribute\Groups;
 use Symfony\Component\Serializer\Attribute\MaxDepth;
 use Symfony\Component\Validator\Constraints as Assert;
@@ -28,6 +29,8 @@ use Symfony\Component\Validator\Constraints as Assert;
 #[ORM\Table(name: 'articles')]
 class Article
 {
+    use TimestampableEntity;
+
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
